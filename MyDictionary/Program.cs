2ae7374fe0724c5391e552a1af688d62aa942ae9
egemenozyurek <ozyurekegemen95@gmail.com>
2021-01-23 15:00:28 +0300

    first commit

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> cities = new MyDictionary<int, string>();
+            cities.Add(06,"Ankara");
+            cities.Add(35,"İzmir");
+            cities.Add(34,"İstanbul");
+
+            cities.GetAll();
+        }
+    }
+}
